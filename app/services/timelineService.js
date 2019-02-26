2437f37a27e104ead844f14b526b8400b500d668
Carl Cronsioe <Carl.Cronsioe@allocatesoftware.com>
2019-02-26 10:42:56 +0100

    removal

--- app/services/timelineService.js
+++ app/services/timelineService.js
@@ -4,16 +4,6 @@ export function getTimeline(requestUrl) {
   return http.get(requestUrl);
 }
 
-export function saveDayType(dayType) {
-  if (dayType.body.id) {
-    const body = { ...dayType.body };
-    delete body.id;
-    return http.put(dayType.requestURL, body);
-  }
-
-  return http.post(dayType.requestURL, dayType.body);
-}
-
 export function saveTimeline(payload) {
   return http.post(payload.requestURL, { body: payload.content });
 }
